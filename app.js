17c92b86ca02a182b6cc1037cbb3bcc7a7a88df0
Ezequiel Portela <eportelab@gmail.com>
2019-08-11 22:09:53 -0300

    helmet & cors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 
 const gameApi = require('./routes/api/game')
 
@@ -11,6 +13,8 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(helmet())
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')))
 
 gameApi(app)
